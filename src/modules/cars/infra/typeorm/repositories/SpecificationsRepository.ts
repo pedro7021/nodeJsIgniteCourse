44e654826ad2cd5fecd052aa8363c79393786cee
Pedro de Carli <pddecarli@hotmail.com>
2022-04-02 21:00:51 -0400

    creation of CreateCarSpecificationUseCase

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -12,15 +12,24 @@ class SpecificationRepository implements ISpecificationsRepository {
         this.repository = getRepository(Specification);
     }
     async findByName(name: string): Promise<Specification> {
-        const specification = this.repository.findOne({ name });
+        const specification = await this.repository.findOne({ name });
         return specification;
     }
     async create({
         name,
         description,
-    }: ICreateSpecificationDTO): Promise<void> {
-        const specification = this.repository.create({ name, description });
+    }: ICreateSpecificationDTO): Promise<Specification> {
+        const specification = await this.repository.create({
+            name,
+            description,
+        });
         await this.repository.save(specification);
+
+        return specification;
+    }
+    async findByIds(ids: string[]): Promise<Specification[]> {
+        const specifications = await this.repository.findByIds(ids);
+        return specifications;
     }
 }
 
---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -0,0 +1,53 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { CarsRepositoryInMemory } from "../../repositories/in-memory/CarsRepositoryInMemory";
+import { SpecificationRepositoryInMemory } from "../../repositories/in-memory/SpecificationRepositoryInMemory";
+import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase";
+
+let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+let specificationRepositoryInMemory: SpecificationRepositoryInMemory;
+describe("Create Car Specification Use Case", () => {
+    beforeEach(() => {
+        carsRepositoryInMemory = new CarsRepositoryInMemory();
+        specificationRepositoryInMemory = new SpecificationRepositoryInMemory();
+        createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
+            carsRepositoryInMemory,
+            specificationRepositoryInMemory
+        );
+    });
+    it("should not be able to add a new specification to inexistent car", async () => {
+        expect(async () => {
+            const car_id = "car_id";
+            const specifications_id = ["specification_id"];
+            await createCarSpecificationUseCase.execute({
+                car_id,
+                specifications_id,
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+    it("should be able to add a new specification to a given car", async () => {
+        const car = await carsRepositoryInMemory.create({
+            name: "Fusca",
+            description: "Description",
+            daily_rate: 100,
+            license_plate: "abcde",
+            fine_amount: 60,
+            brand: "Brand",
+            category_id: "Category",
+        });
+
+        const specification = await specificationRepositoryInMemory.create({
+            name: "Air Conditioning",
+            description: "Air Conditioning",
+        });
+        const specifications_id = [specification.id];
+
+        const specificationsCar = await createCarSpecificationUseCase.execute({
+            car_id: car.id,
+            specifications_id,
+        });
+
+        expect(specificationsCar).toHaveProperty("specifications");
+        expect(specificationsCar.specifications).toHaveLength(1);
+    });
+});
