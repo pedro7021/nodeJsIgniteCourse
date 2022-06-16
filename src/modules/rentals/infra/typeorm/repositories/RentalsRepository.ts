c4dc066b143561e52381af39b959335bf6c0505c
Pedro de Carli <pddecarli@hotmail.com>
2022-06-16 17:01:44 -0400

    Creation of list rentals by user UseCase

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -10,6 +10,7 @@ class RentalsRepository implements IRentalsRepository {
     constructor() {
         this.repository = getRepository(Rental);
     }
+
     async create({
         car_id,
         expected_return_date,
@@ -46,6 +47,12 @@ class RentalsRepository implements IRentalsRepository {
     async findById(id: string): Promise<Rental> {
         const rental = await this.repository.findOne(id);
         return rental;
+    }
+    async findByUser(user_id: string): Promise<Rental[]> {
+        const rentals = await this.repository.find({
+            user_id,
+        });
+        return rentals;
     }
 }
 
