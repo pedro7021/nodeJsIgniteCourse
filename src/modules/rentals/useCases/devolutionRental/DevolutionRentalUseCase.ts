1e0ecb1dd3f07c1b290b196ddbfe80ca8bb43c5b
Pedro de Carli <pddecarli@hotmail.com>
2022-06-16 14:10:27 -0400

    Devolution Rental Controller Creation

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -1,4 +1,4 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import { IDateProvider } from "../../../../shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "../../../../shared/errors/AppError";
@@ -11,6 +11,8 @@ interface IRequest {
     id: string;
     user_id: string;
 }
+
+@injectable()
 class DevolutionRentalUseCase {
     constructor(
         @inject("RentalsRepository")
@@ -22,7 +24,7 @@ class DevolutionRentalUseCase {
     ) {}
     async execute({ id, user_id }: IRequest): Promise<Rental> {
         const rental = await this.rentalsRepository.findById(id);
-        const car = await this.carsRepository.findById(id);
+        const car = await this.carsRepository.findById(rental.car_id);
         const minimum_daily = 1;
         if (!rental) {
             throw new AppError("Rental not found");
