c82dbdf61d214c27343881df317060fb021d054f
Pedro de Carli <pddecarli@hotmail.com>
2022-06-18 21:40:01 -0400

    Creation of RefreshTokenUseCase

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -24,6 +24,20 @@ class UsersTokensRepository implements IUsersTokensRepository {
         await this.repository.save(userToken);
         return userToken;
     }
+
+    async findByUserIdAndRefreshToken(
+        user_id: string,
+        refresh_token: string
+    ): Promise<UserTokens> {
+        const userTokens = await this.repository.findOne({
+            user_id,
+            refresh_token,
+        });
+        return userTokens;
+    }
+    async deleteById(id: string): Promise<void> {
+        await this.repository.delete(id);
+    }
 }
 
 export { UsersTokensRepository };
