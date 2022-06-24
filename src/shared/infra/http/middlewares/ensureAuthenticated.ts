c15ace215369d6d3341c215413cda6dc27fdc6e8
Pedro de Carli <pddecarli@hotmail.com>
2022-06-24 14:46:11 -0400

    Refresh token and ensure authenticated fixes

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -15,7 +15,6 @@ export async function ensureAuthenticated(
     next: NextFunction
 ): Promise<void> {
     const authHeader = request.headers.authorization;
-    const usersTokensRepository = new UsersTokensRepository();
 
     if (!authHeader) {
         throw new AppError("JWT token is missing", 401);
@@ -24,17 +23,9 @@ export async function ensureAuthenticated(
     const [, token] = authHeader.split(" ");
 
     try {
-        const decoded = await verify(token, auth.secret_refresh_token);
+        const decoded = await verify(token, auth.secret_token);
 
         const { sub: user_id } = decoded as IPayload;
-
-        const user = await usersTokensRepository.findByUserIdAndRefreshToken(
-            user_id,
-            token
-        );
-        if (!user) {
-            throw new AppError("User does not exists!", 401);
-        }
 
         request.user = {
             id: user_id,
