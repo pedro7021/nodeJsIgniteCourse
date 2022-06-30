7cc9f9b2d1f6ddddcd79eaa2fe51f6cb1cd7d69d
Pedro de Carli <pddecarli@hotmail.com>
2022-06-30 10:11:34 -0400

    Creation of profileUserUseCase

---
+++ src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
@@ -0,0 +1,18 @@
+import { inject } from "tsyringe";
+
+import { IUserResponseDTO } from "../../dtos/IUserResponseDTO";
+import { User } from "../../infra/typeorm/entities/User";
+import { UserMap } from "../../mapper/UserMap";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+class ProfileUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+    async execute(id: string): Promise<IUserResponseDTO> {
+        const user = await this.usersRepository.findById(id);
+        return UserMap.toDTO(user);
+    }
+}
+export { ProfileUserUseCase };
