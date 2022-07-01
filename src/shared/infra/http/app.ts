c037fc3cf805069def2ff8efd2e690150af2cebe
Pedro de Carli <pddecarli@hotmail.com>
2022-07-01 13:10:38 -0400

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,8 @@ import swaggerUi from "swagger-ui-express";
 
 import "express-async-errors";
 import "../../container";
+import cors from "cors";
+
 import upload from "../../../config/upload";
 import swaggerFile from "../../../swagger.json";
 import { AppError } from "../../errors/AppError";
@@ -22,6 +24,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
